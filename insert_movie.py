c6c8a553e429d98cee12421522e8d57734c9a92a
Jacob Barnum <89816558+jacobtb23@users.noreply.github.com>
2023-02-14 14:28:32 -0700

    Functional Insert_Movie script
    Director table needs to be populated first

--- insert_movie.py
+++ insert_movie.py
@@ -19,33 +19,41 @@ def CreateConnection():
 
 
 def CreateCursor(DataBase):
-    with open('./ITC350-Project/data/movie_data.csv', mode='r') as csv_file:
+    with open('data/movie_data.csv', mode='r') as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         for row in reader:
-            print(" -------------- Movie -----------------")
-            title = row['Title']
-            print(title)
-            release_date = row['Release_Date']
-            print(release_date)
-            imdb_rating = int(row['IMDB_Rating'])
-            print(imdb_rating)
-            movie_genre = row['Genre']
-            print(movie_genre)
-            content_rating = row['Content_Rating']
-            print(content_rating)
-            running_time = int(row['Running_Time'])
-            print(running_time)
-            movie_id = int(row['Movie_ID'])
-            print(movie_id)
-            imdb_link = row['IMDB_Link']
-            print(imdb_link)
-            values = (title, release_date, imdb_rating, movie_genre, content_rating, running_time, movie_id, imdb_link)
-            # values = ('test movie','11-11-2002',1,'test','PG',123,240,'test.com')
-            query = 'INSERT INTO movie_mash_schema.movie (Title, Release_Date, IMDB_Rating, Genre, Content_Rating, ' \
-                    'Running_Time, Movie_ID, IMDB_Link) VALUES (%s, %s, %s, %s, %s, %s, %s, %s )'
-            cursor = DataBase.cursor()
-            cursor.execute(query, values)
-            DataBase.commit()
+            try:
+                print(" -------------- Movie -----------------")
+                title = row['Title']
+                print(title)
+                release_year = row['Release_Date']
+                print(release_year)
+                imdb_rating = int(row['IMDB_Rating'])
+                print(imdb_rating)
+                movie_genre = row['Genre']
+                print(movie_genre)
+                content_rating = row['Content_Rating']
+                print(content_rating)
+                running_time = int(row['Running_Time'])
+                print(running_time)
+                movie_id = int(row['Movie_ID'])
+                print(movie_id)
+                imdb_link = row['IMDB_Link']
+                print(imdb_link)
+                director_id = row['Director_ID']
+                print(director_id)
+                values = (
+                    title, release_year, imdb_rating, movie_genre, content_rating, running_time, movie_id, imdb_link,
+                    director_id)
+                query = 'INSERT INTO movie (Title, Release_Year, IMDB_Rating_Percentage, Genre, Content_Rating, ' \
+                        'Running_Time_Minutes, Movie_ID, IMDB_Link, Director_ID) VALUES (%s, %s, %s, %s, %s, %s, %s, ' \
+                        '%s, ' \
+                        '%s)'
+                cursor = DataBase.cursor()
+                cursor.execute(query, values)
+                DataBase.commit()
+            except Exception as e:
+                print("Error reading in CSV data or pushing to DB: %s" % e)
         DataBase.close()
 
 
