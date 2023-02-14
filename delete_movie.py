821731ade27575aac23cd2b1c253777cee505e1a
berges01 <erik.t.berges@gmail.com>
2023-02-14 13:31:37 -0700

    connects to db

--- delete_movie.py
+++ delete_movie.py
@@ -11,7 +11,7 @@ def CreateConnection():
     DataBase = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="Viva La Vida2009!",
+        passwd="erik1999",
         database="movie_mash",
         port=3306
     )
