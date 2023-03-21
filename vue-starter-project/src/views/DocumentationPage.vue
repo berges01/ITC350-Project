49cab420e12faa8915a65f1a2013abadaa439e91
berges01 <erik.t.berges@gmail.com>
2023-03-21 12:51:40 -0600

    added query lines

--- vue-starter-project/src/views/DocumentationPage.vue
+++ vue-starter-project/src/views/DocumentationPage.vue
@@ -2,6 +2,32 @@
 <template>
     <div>
       <h1>Documentation</h1>
-      <p>Here are some examples of queries and how they work</p>
+      <h2>Here are some examples of queries and how they work</h2>
+      <p align = "left">Select all movies favorited by me</p>
+      <p align = "left">QUERY</p>
+      <p align = "left">Select all movies and sort them by their release date</p>
+      <p align = "left">QUERY</p>
     </div>
   </template>
+
+  
+
+
+<!-- HighlyRatedMovies (Movies Rated over 7) x
+FavoritedByMe x
+SortByReleaseDate x
+SortByRuntime x
+SelectGenre x
+SortMoviesByTitle x
+MoviesWithActor *** Caleb
+MoviesWithAward ***Josh
+MoviesWithDirector ***Josh
+ActorsWithAward *** x Jona x
+AvgMovieRatingByActor x
+AvgMovieRatingByDirector x
+ActorsWithDirector *** x Jona x
+SelectSpecificMovie x
+SelectContentRating x
+MovieRuntimeUnder2hours x
+CustomQueryEditor (Functionality TBD) *** 
+SelectMoviesReleasedBetween x -->
