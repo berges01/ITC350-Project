b345510e5ace006daacb0b7b8ce2398ebe2e22d4
berges01 <erik.t.berges@gmail.com>
2023-04-06 11:34:52 -0600

    consolidated pages

--- vue-starter-project/src/views/LoginPage.vue
+++ vue-starter-project/src/views/LoginPage.vue
@@ -1,21 +1,48 @@
 /* eslint-disable */
 <template>
-    <div>
-      <h1>Login Page</h1>
-      <LoginForm/>
-      This is not working!
+  <h1>Login Page</h1>
+    <div id="login">
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
     </div>
-  </template>
-  <!-- todo get the login stuff working -->
+</template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import LoginForm from '@/components/LoginForm.vue'
-
-export default defineComponent({
+<script>
+export default {
   name: 'LoginPage',
-  components: {
-    LoginForm
+  data () {
+    return {
+      input: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      if (this.input.username !== '' && this.input.password !== '') {
+        if (this.input.username === this.$parent.mockAccount.username && this.input.password === this.$parent.mockAccount.password) {
+          this.$emit('authenticated', true)
+          this.$router.replace({ name: 'secure' })
+        } else {
+          console.log('The username and / or password is incorrect')
+        }
+      } else {
+        console.log('A username and password must be present')
+      }
+    }
   }
-})
+}
 </script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
