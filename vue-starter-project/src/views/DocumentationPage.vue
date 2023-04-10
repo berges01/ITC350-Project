f098d9a7c11073743ee0a9a10edf67e68d1cadc6
berges01 <erik.t.berges@gmail.com>
2023-04-10 13:06:49 -0600

    updated documentation page

--- vue-starter-project/src/views/DocumentationPage.vue
+++ vue-starter-project/src/views/DocumentationPage.vue
@@ -2,6 +2,111 @@
 <template>
     <div>
       <h1>Documentation</h1>
+      <h3 align = "left">Tables</h3>
+      <table class="table">
+<thead>
+  <tr>
+    <th>actors</th>
+    <th>award</th>
+    <th>director</th>
+    <th>favoritedbyfavorite</th>
+    <th>hasactor</th>
+    <th>hasaward</th>
+    <th>movie</th>
+    <th>users</th>
+  </tr>
+  </thead>
+  <tr>
+    <td>Actor_Name</td>
+    <td>Award_Name</td>
+    <td>Director_Name</td>
+    <td>Email</td>
+    <td>Actor_ID</td>
+    <td>Award_ID</td>
+    <td>Title</td>
+    <td>FirstName</td>
+  </tr>
+  <tr>
+    <td>Actor_ID</td>
+    <td>Award_ID</td>
+    <td>Director_ID</td>
+    <td>Movie_ID</td>
+    <td>Movie_ID</td>
+    <td>Movie_ID</td>
+    <td>Movie_ID</td>
+    <td>LastName</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>IMDB_Rating</td>
+    <td>Email</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>Genre</td>
+    <td>Passwd</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>Content_Rating</td>
+    <td>x</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>Running_Time_Minutes</td>
+    <td>x</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>Release_Year</td>
+    <td>x</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>IMDB_Link</td>
+    <td>x</td>
+  </tr>
+  <tr>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>x</td>
+    <td>Director_ID</td>
+    <td>x</td>
+  </tr>
+</table>
       <h3 align = "left">Authors:</h3>
       <p align = "left">Caleb Barton, Erik Berges, Jacob Barnum, Jona Monette, Joshua Halivand</p>
     </div>
