c02727e9ddcf3250e185d9fae96937658d65c795
berges01 <erik.t.berges@gmail.com>
2023-04-11 19:28:26 -0600

    fixed styling of front page

--- vue-starter-project/src/views/HomeView.vue
+++ vue-starter-project/src/views/HomeView.vue
@@ -5,7 +5,7 @@
     <HelloWorld msg="Welcome to Movie Mash!"/>
     <p class="h2">To execute a custom query, use the bar below</p>
       <span class="h5" style = "padding-right: 20px;">Query:</span>
-      <input type="text" v-model="inputText" style="width: 1000px; margin-bottom: 100px; height: 37px;"  placeholder="SELECT * FROM Movies" class="form-row align-items-center h5">
+      <input type="text" v-model="inputText" style="width: 1000px; margin-bottom: 50px; height: 37px;"  placeholder="SELECT * FROM Movies" class="form-row align-items-center h5">
       <button @click="CustomQueryInput" class="btn btn-success">Execute</button>
     <p class="h2">Or try some of our pre-built queries below</p>
     <button @click="sortMoviesByTitle" class="btn btn-success">SortMoviesByTitle</button>
@@ -16,40 +16,54 @@
     <button @click="moviesByDirectorName" class="btn btn-success">SortMoviesByDirectorName</button>
     <button @click="moviesByRating" class="btn btn-success">SortMoviesByRating</button>
     <button @click="moviesByGenre" class="btn btn-success">SortMoviesByGenre</button>
-    <p class="h2">These pre-built queries require input</p>
+    <p class="h2" style="margin-top: 50px;">These pre-built queries require input</p>
     </div>
     <div>
+  <thead>
+  <tr>
+    <th>See your favorite movies</th>
+    <th>See movies given a genre</th>
+    <th>Get Details on a Movie from its ID</th>
+    <th>See what actors have worked with directors</th>
+    <th>See average rating fo a director's movies</th>
+  </tr>
+</thead>
+  <tr>
+    <td><input type="text" v-model="user_id"  placeholder="Email" class="query-boxes">
+    <button @click="favoritedByMe" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="movie_genre"  placeholder="Movie Genre" class="query-boxes">
+    <button @click="moviesWithGenre" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="movie_id" placeholder="Movie_ID" class="query-boxes">
+    <button @click="specificMovie" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="actor_name_director" placeholder="Actor_Name" class="query-boxes">
+    <button @click="actorsWithDirector" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="director_rating" placeholder="Director_Name" class="query-boxes">
+    <button @click="avgDirectorsMoviesRatings" class="btn btn-success">Execute</button></td>
+  </tr>
+
+  <thead>
+  <tr>
+    <th>See average rating of movies with given actor</th>
+    <th>See what awards an actor has won</th>
+    <th>See Movies done by a director</th>
+    <th>See Movies given a content Rating</th>
+  </tr>
+</thead>
+  <tr>
+    <td><input type="text" v-model="user_id"  placeholder="Email" class="query-boxes">
+    <button @click="favoritedByMe" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="movie_genre"  placeholder="Movie Genre" class="query-boxes">
+    <button @click="moviesWithGenre" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="movie_id" placeholder="Movie_ID" class="query-boxes">
+    <button @click="specificMovie" class="btn btn-success">Execute</button></td>
+    <td><input type="text" v-model="actor_name_director" placeholder="Actor_Name" class="query-boxes">
+    <button @click="actorsWithDirector" class="btn btn-success">Execute</button></td>
+  </tr>
+
     <p class="h5">Add movie to favorited list.</p>
     <input type="text" v-model="user_id_favorite"  placeholder="your email" class="query-boxes">
     <input type="text" v-model="movie_id_favorite"  placeholder="movie_id" class="query-boxes">
     <button @click="FavoriteAMovie" class="btn btn-success">Execute</button>
-    <p class="h5">See your favorite movies</p>
-    <input type="text" v-model="user_id"  placeholder="Email" class="query-boxes">
-    <button @click="favoritedByMe" class="btn btn-success">Execute</button>
-    <p class="h5">See movies given a genre</p>
-    <input type="text" v-model="movie_genre"  placeholder="Movie Genre" class="query-boxes">
-    <button @click="moviesWithGenre" class="btn btn-success">Execute</button>
-    <p class="h5">Get Details on a Movie from its ID</p>
-    <input type="text" v-model="movie_id" placeholder="Movie_ID" class="query-boxes">
-    <button @click="specificMovie" class="btn btn-success">Execute</button>
-    <p class="h5">See what actors have worked with directors</p>
-    <input type="text" v-model="actor_name_director" placeholder="Actor_Name" class="query-boxes">
-    <button @click="actorsWithDirector" class="btn btn-success">Execute</button>
-    <p class="h5">See average rating fo a director's movies</p>
-    <input type="text" v-model="director_rating" placeholder="Director_Name" class="query-boxes">
-    <button @click="avgDirectorsMoviesRatings" class="btn btn-success">Execute</button>
-    <p class="h5">See average rating of movies with given actor</p>
-    <input type="text" v-model="actor_rating" placeholder="Actor_Name" class="query-boxes">
-    <button @click="avgActorsMoviesRatings" class="btn btn-success">Execute</button>
-    <p class="h5">See what awards an actor has won</p>
-    <input type="text" v-model="actor_award" placeholder="Actor_Name" class="query-boxes">
-    <button @click="actorsAwards" class="btn btn-success">Execute</button>
-    <p class="h5">See Movies done by a director </p>
-    <input type="text" v-model="director_movies" placeholder="Director_id" class="query-boxes">
-    <button @click="directorsMovies" class="btn btn-success">Execute</button>
-    <p class="h5">See Movies given a content Rating</p>
-    <input type="text" v-model="content_rating" placeholder="Content Rating e.g G,PG..." class="query-boxes">
-    <button @click="moviesOfContentRating" class="btn btn-success">Execute</button>
     <p class="h5">Find movies release between years</p>
     <input type="text" v-model="date1" placeholder="Start year" class="query-boxes">
     <input type="text" v-model="date2" placeholder="End year" class="query-boxes">
@@ -454,6 +468,10 @@ export default {
   width: 250px;
   margin-bottom: 50px;
   height: 37px;
-  text-align: left;
+  margin-left: 50px;
+ }
+
+ .containter{
+  display: flex;
  }
 </style>
